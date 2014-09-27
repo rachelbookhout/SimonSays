05a3ebfe5d5eaa44e46f6972e6ccf1c00b0b8d8e
rachelbookhout <rbookhou@gmail.com>
2014-09-27 12:03:54 -0400

    Built canvas

--- templete.js
+++ templete.js
@@ -4,19 +4,71 @@ function drawRect(x, y, w, h, color) {
 
   ctx.fillRect(x, y, w, h);
 }
-
-function drawCircle(x, y, radius, color) {
-  ctx.strokeStyle = color;
-  ctx.fillStyle = color;
-
+function drawTri1(){
   ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  ctx.moveTo(X_MID,Y_MID);
+  ctx.lineTo(X_MID - 100,Y_MID+100);
+  ctx.lineTo(X_MID,Y_MID + 200);
+  ctx.strokeStyle = "rgb(0, 50, 200)"
+  ctx.fillStyle = "rgb(0, 50, 200)"
   ctx.fill();
 }
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function drawTri2(){
+  ctx.beginPath();
+  ctx.moveTo(X_MID,Y_MID);
+  ctx.lineTo(X_MID + 100, Y_MID -100);
+  ctx.lineTo(X_MID +200,Y_MID);
+  ctx.strokeStyle = "rgb(255, 255, 0)"
+  ctx.fillStyle = "rgb(255, 255, 0)"
+  ctx.fill();
 }
+function drawTri3(){
+ ctx.beginPath();
+  ctx.moveTo(X_MID + 200,Y_MID);
+  ctx.lineTo(X_MID + 300, Y_MID + 100);
+  ctx.lineTo(X_MID + 200 ,Y_MID + 200);
+  ctx.strokeStyle = "rgb(0, 255, 7)"
+  ctx.fillStyle = "rgb(0, 255, 7)"
+  ctx.fill();
+}
+function drawTri4(){
+ctx.beginPath();
+  ctx.moveTo(X_MID + 200,Y_MID + 200);
+  ctx.lineTo(X_MID +100, Y_MID + 300);
+  ctx.lineTo(X_MID , Y_MID  +200);
+  ctx.strokeStyle = "rgb(215, 40, 40)"
+  ctx.fillStyle = "rgb(215, 40, 40)"
+  ctx.fill();
+
+}
+
+
+
+
+// function drawDi(){
+//   ctx.beginPath();
+//   ctx.moveTo(75,0);
+//   ctx.lineTo(150,100);
+//   ctx.lineTo(75,200);
+//   ctx.lineTo(0,100);
+//   ctx.strokeStyle = "rgb(0,200,50)"
+//   ctx.fillStyle = "rgb(0,200,50)"
+//   ctx.fill();
+// }
+
+
+// function drawCircle(x, y, radius, color) {
+//   ctx.strokeStyle = color;
+//   ctx.fillStyle = color;
+
+//   ctx.beginPath();
+//   ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+//   ctx.fill();
+// }
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
@@ -24,103 +76,112 @@ var ctx = canvas.getContext('2d');
 ctx.canvas.width = window.innerWidth * 0.95;
 ctx.canvas.height = window.innerHeight * 0.95;
 
+
+
 var SCREEN_WIDTH = ctx.canvas.width;
 var SCREEN_HEIGHT = ctx.canvas.height;
-
-var ball = {
-  x: SCREEN_WIDTH / 2,
-  y: SCREEN_HEIGHT / 2,
-  radius: SCREEN_HEIGHT / 50,
-  xVel: SCREEN_WIDTH / 100,
-  isStopped: false
-};
+var X_MID = SCREEN_WIDTH/2.5
+var Y_MID = SCREEN_HEIGHT/3
+// var ball = {
+//   x: SCREEN_WIDTH / 2,
+//   y: SCREEN_HEIGHT / 2,
+//   radius: SCREEN_HEIGHT / 50,
+//   xVel: SCREEN_WIDTH / 100,
+//   isStopped: false
+// };
 
 function draw() {
   ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
   // Draw elements of the game here.
   // Can use `drawRect` and `drawCircle` function.
   // e.g. draw the ball colored green
-  drawCircle(ball.x, ball.y, ball.radius, 'green');
+  drawRect(X_MID, Y_MID, 200,200, 'grey');
+  drawTri1();
+  drawTri2();
+  drawTri3();
+  drawTri4();
+
 }
 
-function tick() {
-  // Update game logic here.
-  // e.g. moving a ball back and forth across the screen
+// function tick() {
+//   // Update game logic here.
+//   // e.g. moving a ball back and forth across the screen
 
-  if (!ball.isStopped) {
+//   if (!ball.isStopped) {
 
-    ball.x += ball.xVel;
+//     ball.x += ball.xVel;
 
-    // Reverse when ball hits right or left side of the screen.
-    if ((ball.xVel > 0 && (ball.x + ball.radius) > SCREEN_WIDTH ||
-         (ball.xVel < 0 && (ball.x - ball.radius) < 0))) {
-      ball.xVel = -ball.xVel;
-    }
-  }
-}
+//     // Reverse when ball hits right or left side of the screen.
+//     if ((ball.xVel > 0 && (ball.x + ball.radius) > SCREEN_WIDTH ||
+//          (ball.xVel < 0 && (ball.x - ball.radius) < 0))) {
+//       ball.xVel = -ball.xVel;
+//     }
+//   }
+// }
 
-function loop(time) {
-  tick();
-  draw();
+// function loop(time) {
+//   tick();
+//   draw();
 
-  window.requestAnimationFrame(function(time) {
-    loop(time);
-  });
-}
+//   window.requestAnimationFrame(function(time) {
+//     loop(time);
+//   });
+// }
 
-function keyDown(event) {
-  var handled = true;
+// function keyDown(event) {
+//   var handled = true;
 
-  switch (event.keyCode) {
+//   switch (event.keyCode) {
 
-  // Handle user input here when a key is pressed.
-  // A few key constants are defined in /js/key_codes.js
-  // e.g. stops the ball from moving when pressing down the space bar
+//   // Handle user input here when a key is pressed.
+//   // A few key constants are defined in /js/key_codes.js
+//   // e.g. stops the ball from moving when pressing down the space bar
 
-  case SPACE_KEY:
-    ball.isStopped = true;
-    break;
+//   case SPACE_KEY:
+//     ball.isStopped = true;
+//     break;
 
-  default:
-    handled = false;
-    break;
-  }
+//   default:
+//     handled = false;
+//     break;
+//   }
 
-  if (handled) {
-    event.preventDefault();
-  }
-}
+//   if (handled) {
+//     event.preventDefault();
+//   }
+// }
 
-function keyUp(event) {
-  var handled = true;
+// function keyUp(event) {
+//   var handled = true;
 
-  switch (event.keyCode) {
+//   switch (event.keyCode) {
 
-  // Handle user input here when a key is released.
-  // A few key constants are defined in /js/key_codes.js
-  // e.g. starts the ball moving again when releasing space bar
+//   // Handle user input here when a key is released.
+//   // A few key constants are defined in /js/key_codes.js
+//   // e.g. starts the ball moving again when releasing space bar
 
-  case SPACE_KEY:
-    ball.isStopped = false;
-    break;
+//   case SPACE_KEY:
+//     ball.isStopped = false;
+//     break;
 
-  default:
-    handled = false;
-    break;
-  }
+//   default:
+//     handled = false;
+//     break;
+//   }
 
-  if (handled) {
-    event.preventDefault();
-  }
-}
+//   if (handled) {
+//     event.preventDefault();
+//   }
+// }
 
-function run() {
-  window.onkeydown = keyDown;
-  window.onkeyup = keyUp;
+// function run() {
+//   window.onkeydown = keyDown;
+//   window.onkeyup = keyUp;
 
-  window.requestAnimationFrame(function(time) {
-    loop(time);
-  });
-}
+//   window.requestAnimationFrame(function(time) {
+//     loop(time);
+//   });
+// }
 
-run();
+// run();
+draw();
